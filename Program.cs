d994d7d4bc4e06419c34aa7cf1c66abed7711ba4
E2E Test <test@e2e.local>
2026-04-09 14:48:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapRazorComponents<App.Components.App>()
+    .AddInteractiveServerRenderMode();
+app.Run();
